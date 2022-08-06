0f38259b421c3aa13926d54dbc62cbe5c1fbe1be
modihj <106487940+modihj@users.noreply.github.com>
2022-08-06 11:00:19 -0400

    Add files via upload

---
+++ Lab_04_01_Sales_Tax.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+   public class Lab_04_01_Sales_Tax {
+
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Enter price of purchase: $");
+            double price = in.nextDouble();
+            double tax = price * 0.05;
+            System.out.printf("Sales tax is $%.2f\n", tax);
+        }
+
+    }
+
+
